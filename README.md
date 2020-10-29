77d68315c106ab644b20cbb99e9ded0dc6ec9858
mora5925 <wahba190@gmail.com>
2020-10-29 15:00:44 -0400

    burger

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Burger
+
+ Burger using  MySQL, Node, Express, Handlebars and a homemade ORM. You can type in a burger from the menu, add it, and then devour it.
+
+ Burger is an application which allows users to input the names of burgers they'd like to eat from the menu.
+
+When the user submits a burger's name, the application will display the burger on the left side of the page while it waits to be devoured.
+ Burger stores every burger in Dbeaver database
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,60 @@
+
+$(function () {
+  $(".devour-it").on("click", function (event) {
+    var id = $(this).data("id");
+    var devoured = $(this).data("devoured");
+
+    var eaten = {
+      devoured: devoured
+    };
+
+  
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: eaten
+    }).then(
+      function () {
+        console.log("Burger got devoured", devoured);
+      
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function (event) {
+    
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#bu").val().trim(),
+      devoured: false
+    };
+
+   
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function () {
+        console.log("Added a New Burger");
+        
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function (event) {
+    var id = $(this).data("id");
+
+   
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function () {
+        console.log("deleted burger", id);
+        
+        location.reload();
+      }
+    );
+  });
+});
