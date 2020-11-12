1f53118b239a913c61d08904850796d7a9688cef
mora5925 <wahba190@gmail.com>
2020-11-11 20:13:05 -0500

    heroku problem

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+  
 var express = require("express");
 
-var PORT = process.env.PORT || 3001;
+var PORT = process.env.PORT || 8080;
 
 var app = express();
 
 
 app.use(express.static("public"));
+app.use(express.static("public/assets/img"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -16,13 +19,13 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
+// Import routes and give the server access to them.
 var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
-
+// Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
-  
+  // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
