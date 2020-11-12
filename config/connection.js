ef4389d1e8b25e7ed770fbb8bf396a34c0a05cc2
mora5925 <wahba190@gmail.com>
2020-11-11 19:01:06 -0500

    fixing

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,44 @@
-var mysql = require("mysql");
 
-var connection;
+// create connection to our db
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    });
 
-if (process.env.JAWSDB_URL){
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Paviliondv731$",
-    database: "burgers_db"
-  });
-};
+module.exports = sequelize;
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-module.exports = connection;
+
+
+
+
+
+
+
+
+// var mysql = require("mysql");
+
+// var connection;
+
+// if (process.env.JAWSDB_URL){
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "Paviliondv731$",
+//     database: "burgers_db"
+//   });
+// };
+
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+// module.exports = connection;
