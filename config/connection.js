f5762a928b01275432b52c7fb36892a61866b575
mora5925 <wahba190@gmail.com>
2020-11-11 19:24:48 -0500

    heroku problem

--- config/connection.js
+++ config/connection.js
@@ -1,47 +1,24 @@
-const Sequelize = require('sequelize');
+var mysql = require("mysql");
 
-require('dotenv').config();
+var connection;
 
-// create connection to our db
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    });
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Paviliondv731$",
+    database: "burgers_db"
+  });
+};
 
-module.exports = sequelize;
-
-
-
-
-
-
-
-
-
-// var mysql = require("mysql");
-
-// var connection;
-
-// if (process.env.JAWSDB_URL){
-//   connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "Paviliondv731$",
-//     database: "burgers_db"
-//   });
-// };
-
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-// module.exports = connection;
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+module.exports = connection;
--- server.js
+++ server.js
@@ -3,23 +3,26 @@ var express = require("express");
 var PORT = process.env.PORT || 8080;
 
 var app = express();
-const sequelize = require("./config/connection");
+
 
 app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
 
 var routes = require("./controllers/burgersController.js");
 
 app.use(routes);
 
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+app.listen(PORT, function() {
+  
+  console.log("Server listening on: http://localhost:" + PORT);
 });
